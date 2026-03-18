58b3c432db238c78176c3139b81ca395458b24e3
jibz95 <jbchaps@gmail.com>
2026-03-18 15:36:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-START_URL = "https://www.premier-service.fr/_start/ics.php"
 CLUB_ID = "32920393"
-
 LOGIN = os.environ.get("TENNIS_LOGIN", "JECHAP")
 PASSWORD = os.environ.get("TENNIS_PASSWORD", "")
 PARTNER_VALUE = "-100"  # Aurelien LANGE
@@ -19,68 +17,42 @@ def get_md5(s):
     return hashlib.md5(s.upper().encode()).hexdigest()
 
 
-def extract_fields(html):
-    """Extrait les champs dynamiques depuis le HTML du formulaire de login."""
-    fl = fp = fm = idpge_val = action_url = None
-
-    # Champ identifiant (input type=text visible, pas userid)
-    m = re.search(r'<input[^>]+type=["\']text["\'][^>]+name=["\'](\w+)["\']', html)
-    if m and m.group(1) not in ("userid",):
-        fl = m.group(1)
-
-    # Champ password
-    m = re.search(r'<input[^>]+type=["\']password["\'][^>]+name=["\'](\w+)["\']', html)
-    if m and m.group(1) not in ("userkey",):
-        fp = m.group(1)
-
-    # Champ MD5 : hidden sans value, pas dans la liste fixe
-    fixed = {"idact", "usermd5", "idgfcmiid", "largeur_ecran", "hauteur_ecran",
-             "pingmax", "pingmin", "userid", "userkey", "idses"}
-    for m in re.finditer(r'<input[^>]+type=["\']hidden["\'][^>]+name=["\'](\w+)["\'][^>]*>', html):
-        name = m.group(1)
-        val_m = re.search(r'value=["\']([^"\']*)["\']', m.group(0))
-        val = val_m.group(1) if val_m else ""
-        if name not in fixed and not val:
-            fm = name
-            break
-
-    # idpge
-    m = re.search(r'name=["\']idpge["\'][^>]+value=["\']([^"\']+)["\']', html)
-    if m:
-        idpge_val = m.group(1)
-
-    # action URL du form
-    m = re.search(r'<form[^>]+action=["\']([^"\']+)["\']', html)
-    if m:
-        action_url = m.group(1)
-
-    return fl, fp, fm, idpge_val, action_url
+def make_session():
+    s = requests.Session()
+    s.headers.update({
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+        "Content-Type": "application/x-www-form-urlencoded",
+    })
+    return s
 
 
 def login():
-    session = requests.Session()
-    session.headers.update({
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Origin": "https://www.premier-service.fr",
-    })
+    session = make_session()
 
-    # Charger la page de login via adsltennis
+    # Étape 1 : charger la page _start pour obtenir le PHPSESSID
     r0 = session.get(
         f"https://www.adsltennis.fr/_start/index.php?club={CLUB_ID}&idact=101",
         allow_redirects=True
     )
+
+    # La page adsltennis redirige vers premier-service.fr/_start/index.php
+    # qui contient le vrai formulaire de login
     html = r0.text
+
+    # Extraire les champs depuis le HTML
     fl, fp, fm, idpge_val, action_url = extract_fields(html)
 
-    # Fallback : essayer l'URL _start directement
-    if not fl and not fm:
-        r1 = session.get(START_URL, params={"club": CLUB_ID, "idact": "101"})
-        html = r1.text
-        fl, fp, fm, idpge_val, action_url = extract_fields(html)
-
     md5 = get_md5(PASSWORD + LOGIN)
-    post_url = action_url if action_url and action_url.startswith("http") else START_URL
+
+    # Construire l'URL de POST
+    if action_url:
+        if action_url.startswith("http"):
+            post_url = action_url
+        else:
+            base = r0.url.rsplit("/", 1)[0]
+            post_url = base + "/" + action_url.lstrip("/")
+    else:
+        post_url = "https://www.premier-service.fr/_start/ics.php"
 
     payload = {
         "idact": "101",
@@ -98,22 +70,79 @@ def login():
     if fp: payload[fp] = ""
     if fm: payload[fm] = md5
 
+    session.headers["Referer"] = r0.url
+    session.headers["Origin"] = "https://www.premier-service.fr"
+
     resp = session.post(post_url, data=payload)
-    connected = "fiche_identification" not in resp.text and "fiche_erreur" not in resp.text
+    connected = ("fiche_identification" not in resp.text
+                 and "fiche_erreur" not in resp.text
+                 and "404" not in resp.text[:200])
 
     debug = {
         "field_login": fl, "field_md5": fm,
         "idpge_val": idpge_val, "post_url": post_url,
+        "action_url": action_url,
         "md5": md5, "r0_url": r0.url,
-        "html_snippet": html[html.find("<form"):html.find("<form")+500] if "<form" in html else "no form",
+        "r0_length": len(html),
         "cookies": dict(session.cookies),
         "connected": connected,
+        "resp_url": resp.url,
+        "resp_length": len(resp.text),
     }
     return session, resp, debug
 
 
+def extract_fields(html):
+    fl = fp = fm = idpge_val = action_url = None
+
+    # Action URL
+    m = re.search(r'<form[^>]+action=["\']([^"\']+)["\']', html, re.I)
+    if m:
+        action_url = m.group(1)
+
+    # Champ login (text visible, pas userid)
+    for m in re.finditer(r'<input[^>]+type=["\']text["\'][^>]*>', html, re.I):
+        nm = re.search(r'name=["\'](\w+)["\']', m.group(0))
+        if nm and nm.group(1) not in ("userid",):
+            fl = nm.group(1)
+            break
+
+    # Champ password (pas userkey)
+    for m in re.finditer(r'<input[^>]+type=["\']password["\'][^>]*>', html, re.I):
+        nm = re.search(r'name=["\'](\w+)["\']', m.group(0))
+        if nm and nm.group(1) not in ("userkey",):
+            fp = nm.group(1)
+            break
+
+    # Champ MD5 : hidden sans valeur, hors liste fixe
+    fixed = {"idact", "usermd5", "idgfcmiid", "largeur_ecran", "hauteur_ecran",
+             "pingmax", "pingmin", "userid", "userkey", "idses", "b_i", "club"}
+    for m in re.finditer(r'<input[^>]+type=["\']hidden["\'][^>]*>', html, re.I):
+        nm = re.search(r'name=["\'](\w+)["\']', m.group(0))
+        if not nm:
+            continue
+        name = nm.group(1)
+        vm = re.search(r'value=["\']([^"\']*)["\']', m.group(0))
+        val = vm.group(1) if vm else ""
+        if name not in fixed and not val:
+            fm = name
+            break
+
+    # idpge
+    m = re.search(r'name=["\']idpge["\'][^>]+value=["\']([^"\']+)["\']', html, re.I)
+    if not m:
+        m = re.search(r'value=["\']([^"\']+)["\'][^>]+name=["\']idpge["\']', html, re.I)
+    if m:
+        idpge_val = m.group(1)
+
+    return fl, fp, fm, idpge_val, action_url
+
+
 def get_planning(session, date_str):
-    return session.post(START_URL, data={"idact": "345", "ladate": date_str, "idses": "S0"})
+    return session.post(
+        "https://www.premier-service.fr/_start/ics.php",
+        data={"idact": "345", "ladate": date_str, "idses": "S0"}
+    )
 
 
 def parse_slots(html):
@@ -129,13 +158,14 @@ def parse_slots(html):
 
 
 def validate_reservation(session, idpge):
-    session.post(START_URL, data={
+    url = "https://www.premier-service.fr/_start/ics.php"
+    session.post(url, data={
         "idact": "332", "idpge": idpge,
         "IDOBJ": "100", "idpar": "100",
         "CHAMP_TYPE_1": PARTNER_VALUE,
         "idses": "S0", "b_i": "0",
     })
-    return session.post(START_URL, data={
+    return session.post(url, data={
         "idact": "366", "idpge": idpge,
         "idses": "S0", "b_i": "0",
     })
@@ -152,7 +182,25 @@ def debug_login():
     return jsonify({
         "connected": debug["connected"],
         "debug": debug,
-        "html_preview": resp.text[:1500],
+        "html_preview": resp.text[:2000],
+    })
+
+
+@app.route("/debug-r0")
+def debug_r0():
+    """Voir le HTML brut de la page de login après redirections."""
+    session = make_session()
+    r0 = session.get(
+        f"https://www.adsltennis.fr/_start/index.php?club={CLUB_ID}&idact=101",
+        allow_redirects=True
+    )
+    html = r0.text
+    return jsonify({
+        "url": r0.url,
+        "length": len(html),
+        "has_form": "<form" in html,
+        "form_snippet": html[html.find("<form"):html.find("<form")+1500] if "<form" in html else "no form",
+        "html_start": html[:500],
     })
 
 
